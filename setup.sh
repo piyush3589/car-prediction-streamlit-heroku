e40844c9dc8401161415c2ddbc8eba558348a396
Piyush-Raut <99467258+Piyush-Raut@users.noreply.github.com>
2022-04-02 13:12:59 +0530

    second commit

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"email@domain\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
+port = $PORT\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
